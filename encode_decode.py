a7e9d79dea6abc18bd5c1ff155223daf4872e794
unknown <dylanmb331@gmail.com>
2023-10-31 23:40:45 -0400

    boes added decode function and lightly edited

--- encode_decode.py
+++ encode_decode.py
@@ -1,33 +1,41 @@
 #functions
 
+#this was coded by yona
+#some of the text was edited by sky (dylan) boes
 def menu():
     print("Menu")
     print("-------------")
-    print("1. Encode\n2. Decode\n3. Quit")
+    print("1. Encode\n2. Decode\n3. Quit\n")
 
 def encode(password):
-    global orig_pass
     global encoded_password
     if len(password) != 8 and not password.isdigit():
         print("Password must be 8-digits and only consist of numbers")
 
     else:
-        orig_pass = password
         encoded_password = ''.join(str((int(digit) + 3) % 10) for digit in password)
-        print('Your password has been encoded and stored')
-    
+        print('Your password has been encoded and stored!')
+
+#this was coded by sky (dylan) boes
+def decode(password):
+  decoded_password = ''.join(str((int(digit) - 3) % 10) for digit in password)
+  return decoded_password
+
+#this was coded by yona
+#sky (dylan) boes edited some of the text, initialized encoded_password, and made the code work with the decode() function
+
 if __name__ == "__main__":
     while True:
         menu()
         choice = input("Please enter an option: ")
+        encoded_password = ""
 
         if choice == "1":
-            password = input("Please enter a password to be encoded that is 8 chars long and only digits: ")
+            password = input("Please enter your password to encode: ")
             encode(password)
         elif choice == "2":
-            print("The encoded password is " + encoded_password + ", and the original password is " + orig_pass)
+            print("The encoded password is " + encoded_password + ", and the original password is " + decode(encoded_password) + ".\n")
         elif choice == "3":
-            print("Thank you for using this password encoder/decoder. Have a nice day!")
             break
         else:
             print("Please enter a value 1-3")
